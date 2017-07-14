c929ab6ddcd2152395f1937d301d840813c5e07a
anderson.bertoni.ext <anderson.bertoni@sptrans.com.br>
2017-07-14 16:58:54 -0300

    Angular20Example
    Inclusão do Projeto Angular20Example

---
+++ Angular20Example/app/main.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>Hello World!</h1>
+    `
+})
+
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
